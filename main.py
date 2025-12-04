5dfee7fc22c2bd0d6ab68a026dfd88644d2b2366
Adrienne <adriennemedenilla@gmail.com>
2025-12-04 13:56:06 +0800

    new

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+
+def main():
+  print("------Hellow, Welcome to my PAWN SHOP MEOW!------")
+  while True:
+    princicpal = int(input("Enter Principal Amount: \n"))
+    days = int(input("Enter number of Days after Loan: \n"))
+    
+    print("Choose Transation:\n")
+    print("A) Renew / Extend Loan (Pay Interest + Service Fee of P10)")
+    print("B) Redeem / Recover collateral ")
+    
+    
+  
+  pass
+
+
+if __name__ == "__main__":
+    main()
